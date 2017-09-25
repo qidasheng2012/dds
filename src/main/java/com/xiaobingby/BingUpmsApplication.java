8fc0576af7f11777812cb180ddff58c235c64321
XiaoBingBy <XiaoBingBy@qq.com>
2017-09-26 00:27:28 +0800

    启动类

---
+++ src/main/java/com/xiaobingby/BingUpmsApplication.java
@@ -0,0 +1,12 @@
+package com.xiaobingby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingUpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingUpmsApplication.class, args);
+	}
+}
