48eed71c3f69c06cbd732fbc36667a8121c8cb7c
XiaoBingBy <XiaoBingBy@qq.com>
2017-09-26 00:25:10 +0800

    全局异常处理 处理Shiro注解跳转

---
+++ src/main/java/com/xiaobingby/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.xiaobingby.config.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Author: XiaoBingBy
+ * Email: XiaoBingBy@qq.com
+ * Date: 2017/9/1
+ * Time: 00:00
+ * Describe: 全局异常处理 处理Shiro注解跳转
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = org.apache.shiro.authz.UnauthorizedException.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
+        response.sendRedirect("/500.html");
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName("admin/403");
+        return mav;
+    }
+
+}
