5370aa6635f7b371ad51b4979271605d380e073f
XiaoBingBy <XiaoBingBy@qq.com>
2017-09-29 10:03:16 +0800

    添加 发送mail

---
+++ src/test/java/com/xiaobingby/mail/MailTest.java
@@ -0,0 +1,39 @@
+package com.xiaobingby.mail;
+
+import com.xiaobingby.BingUpmsApplicationTests;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Author: XiaoBingBy
+ * Email: XiaoBingBy@qq.com
+ * Date: 2017/9/29
+ * Time: 9:57
+ * Describe:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class MailTest {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void testMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("fanshuye1304@163.com");//发送者.
+        message.setTo("867510022@qq.com");//接收者.
+        message.setSubject("测试邮件（邮件主题）");//邮件主题.
+        message.setText("这是邮件内容");//邮件内容.
+
+        mailSender.send(message);//发送邮件
+    }
+
+}
